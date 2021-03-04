b5448bbbc785fd5bc4843ee5b96a1044fedc4f6c
Eduard Cot Ros <eduardcot@iMac-de-Eduard.local>
2021-03-04 18:03:45 +0100

    change

--- src/pages/escriptura.vue
+++ src/pages/escriptura.vue
@@ -5,7 +5,7 @@
 
     <ItemList 
       src="/portada-babieca.png"
-      title="Constelació Babieca"
+      title="Constel·lació Babieca"
       type="Novelette"
       date="maig 2016"
       text="Un expert en seguretat informàtica, amb problemes amb la justícia, veu amb impotència com la seva vida s'enfonsa de manera inevitable. Una nit, per casualitat, topa amb una noia que ha estat víctima d'un phishing que ha fructificat en un intent de violació. Investigant qui hi ha darrere d'aquest engany, Linus descobrirà una xarxa criminal molt més gran del que semblava al principi, i en conseqüència, una via per poder ajudar-se, també a si mateix, a redimir-se amb el sistema judicial."
